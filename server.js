61c394fd586e447fabb5ddf0956bda1943a12de5
devopscomputer <pauloti.engenharia@gmail.com>
2026-02-13 19:24:05 -0300

    update: update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { initSchema } from './db.js';
 import { startPriceWorker } from './workers/priceWorker.js';
 import { startOnchainWorker } from './workers/onchainWorker.js';
 import { startPayoutWorker } from './workers/payoutWorker.js';
+import { startSweepWorker } from './workers/sweepWorker.js';
 import { logger } from './logger.js';
 
 (async () => {
@@ -10,6 +11,7 @@ import { logger } from './logger.js';
   startPriceWorker();
   startOnchainWorker();
   startPayoutWorker();
+  startSweepWorker();
   const port = process.env.PORT || 3000;
   app.listen(port, () => logger.info({ port }, 'Server started'));
 })();
