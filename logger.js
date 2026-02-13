10159ceb312f315cb17bf4b5afbfaa10da6149fa
devopscomputer <pauloti.engenharia@gmail.com>
2026-02-13 14:02:30 -0300

    update: update logger.js

---
+++ logger.js
@@ -0,0 +1,9 @@
+import pino from 'pino';
+import pinoHttp from 'pino-http';
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  transport: process.env.NODE_ENV === 'development' ? { target: 'pino-pretty' } : undefined
+});
+
+export const httpLogger = pinoHttp({ logger });
